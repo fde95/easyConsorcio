ed7b9bfa20d4f5ae616c421f1e557da699b70cb0
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-06-20 11:20:32 -0300

    Finaizando AboutUs e criando Subscribe e Products

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
         duration: 3000,
         once: true,
     });
+
+    $('#info').ready(function(){
+        AOS.init({
+            duration: 2500,
+            once: false,
+        });
+    })
 })
