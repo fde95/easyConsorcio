de5e73d0d050c08dcddc85a224428464d149eeff
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-06-20 14:48:29 -0300

    Ajustando Responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
     AOS.init({
-        duration: 3000,
-        once: true,
     });
 
-    $('#info').ready(function(){
-        AOS.init({
-            duration: 2500,
-            once: false,
-        });
-    })
 })
